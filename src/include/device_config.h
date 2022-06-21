7e65b67c8f3670cb04026aea897f496003ea5c89
NS-SpartanLabs <100725539+NS-SpartanLabs@users.noreply.github.com>
2022-06-20 21:54:41 -0400

    update v2

--- src/include/device_config.h
+++ src/include/device_config.h
@@ -8,14 +8,14 @@
 */
 
 // Copy the value from Device EUI from the TTN console in LSB mode.
-static const u1_t PROGMEM DEVEUI[8]= { }; // Device EUI, hex, lsb
+static const u1_t PROGMEM DEVEUI[8]= { 0xA3, 0x24, 0x05, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 }; // Device EUI, hex, lsb
 
 // Copy the value from Application EUI from the TTN console in LSB mode
-static const u1_t PROGMEM APPEUI[8]= { ; // Application EUI, hex, lsb
+static const u1_t PROGMEM APPEUI[8]= {  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; // Application EUI, hex, lsb
 
 /*  This key should be in big endian format (or, since it is not really a
     number but a block of memory, endianness does not really apply). In
     practice, a key taken from ttnctl can be copied as-is. Anyway its in MSB mode.
 */
-static const u1_t PROGMEM APPKEY[16] = { }; // App Key, hex, msb
+static const u1_t PROGMEM APPKEY[16] = { 0xC8, 0x7A, 0xB5, 0x7F, 0x84, 0x33, 0xA7, 0x15, 0x1F, 0x3B, 0x25, 0x43, 0x60, 0x84, 0x5C, 0xB3 }; // App Key, hex, msb
 
--- src/src/main.cpp
+++ src/src/main.cpp
@@ -3,8 +3,8 @@
     - Reads sensor values from BME280 and sends it to TTN
     - ...
     
-    author: Marco Roda, marcoroda.com
-    date:   Mar 2020
+    author: Nicholas Singleton, SpartanLabs
+    date:   June 2022
 */
 
 #include <Arduino.h>
@@ -12,7 +12,7 @@
 #include <hal/hal.h>
 #include <SPI.h>
 #include <SSD1306.h>
-#include "_device_config.h" /* Replace by device_config.h and add TTN keys to it */
+#include "device_config.h" /* Replace by device_config.h and add TTN keys to it */
 
 #include "Bme280.h"
 
@@ -289,15 +289,15 @@ void setup(void)
         Setting up channels should happen after LMIC_setSession, as that
         configures the minimal channel set.
     */
-    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    // LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    // LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
     
     /*  TTN defines an additional channel at 869.525Mhz using SF9 for class B
         devices' ping slots. LMIC does not have an easy way to define set this
@@ -305,13 +305,13 @@ void setup(void)
         frequency is not configured here.
     */
     // Disable link check validation
-    LMIC_setLinkCheckMode(0);
+    // LMIC_setLinkCheckMode(0);
 
     // TTN uses SF9 for its RX2 window.
-    LMIC.dn2Dr = DR_SF9;
+    // LMIC.dn2Dr = DR_SF9;
 
     // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
-    LMIC_setDrTxpow(DR_SF11,14);
+    // LMIC_setDrTxpow(DR_SF11,14);
     // LMIC_setDrTxpow(DR_SF7,14);
 
     /* Start job */
